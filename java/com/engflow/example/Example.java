614afe5b71aad94328b28582ae5282df8d714f04
Mike Z <mike@engflow.com>
2022-09-05 15:55:03 -0700

    First commit

---
+++ java/com/engflow/example/Example.java
@@ -0,0 +1,18 @@
+package com.engflow.example;
+
+public class Example {
+  public static String fizzbuzz(int i) {
+    if (i % 3 == 0) {
+      if (i % 5 == 0) {
+        return "FizzBuzz";
+      }
+      return "Fizz";
+    } else if (i % 5 == 0) {
+      return "Buzz";
+    } else {
+      return Integer.toString(i);
+      
+
+    }
+  }
+}
